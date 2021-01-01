71035f20b05382a162b99a5bc3a70598c86b6fa7
yuri.miya <yuri.miya@yahoo.co.jp>
2021-01-02 00:42:31 +0900

    Add Inventory class

---
+++ src/models/Inventory.java
@@ -0,0 +1,133 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+
+@Table(name="inventories")
+@NamedQueries({
+    @NamedQuery(
+            name="getAllInventories",
+            query="SELECT e FROM Inventory AS e ORDER BY e.id DESC"
+            ),
+    @NamedQuery(
+            name="getInventories",
+            query="SELECT COUNT (e) FROM Inventory AS e"
+            ),
+    @NamedQuery(
+            name="checkRegisteredTrade_code",
+            query="SELECT COUNT (e) FROM Inventory AS e WHERE e.trade_code"
+            )
+
+})
+@Entity
+public class Inventory {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "trade_code", nullable = false, unique = true)
+    private String trade_code;
+
+    @Column(name = "trade_name", nullable = false)
+    private String trade_name;
+
+    @Column(name = "order_flag", nullable = false)
+    private Integer order_flag;
+
+    @Column(name = "receiving", nullable = false)
+    private Integer receiving;
+
+    @Column(name = "shiping", nullable = false)
+    private Integer shiping;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
+    @Column(name = "delete_flag", nullable = false)
+    private Integer delete_flag;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTrade_code() {
+        return trade_code;
+    }
+
+    public void setTrade_ode(String trade_code) {
+        this.trade_code = trade_code;
+    }
+
+    public String getTrade_name() {
+        return trade_name;
+    }
+
+    public void setTrade_name(String trade_name) {
+        this.trade_name = trade_name;
+    }
+
+    public Integer getOrder_flag() {
+        return order_flag;
+    }
+
+    public void setOrder_flag(Integer order_flag) {
+        this.order_flag = order_flag;
+    }
+
+    public Integer getReceiving() {
+        return receiving;
+    }
+
+    public void setReceiving(Integer receiving) {
+        this.receiving = receiving;
+    }
+
+    public Integer getShiping() {
+        return shiping;
+    }
+
+    public void setShiping(Integer shiping) {
+        this.shiping = shiping;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public Integer getDelete_flag() {
+        return delete_flag;
+    }
+
+    public void setDelete_flag(Integer delete_flag) {
+        this.delete_flag = delete_flag;
+    }
+}
---
+++ src/models/validators/InventoryValidator.java
@@ -0,0 +1,54 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import models.Inventory;
+import utils.DBUtil;
+
+public class InventoryValidator {
+    public static List<String> validate(Inventory e,Boolean trade_codeDuplicateCheckFlag){
+        List<String> errors = new ArrayList<String>();
+
+        String trade_code_error=validateTrade_code(e.getTrade_code(),trade_codeDuplicateCheckFlag);
+        if(!trade_code_error.equals("")){
+            errors.add(trade_code_error);
+        }
+
+        String trade_name_error=validateTrade_name(e.getTrade_name());
+        if(!trade_name_error.equals("")){
+            errors.add(trade_name_error);
+        }
+
+        return errors;
+    }
+
+    private static String validateTrade_code(String trade_code,Boolean trade_codeDuplicateCheckFlag){
+        if(trade_code==null || trade_code.equals("")){
+            return "商品番号を入力してください。";
+        }
+
+        if(trade_codeDuplicateCheckFlag){
+            EntityManager em = DBUtil.createEntityManager();
+            long inventories_count=(long)em.createNamedQuery("checkRegisteredTrade_code",Long.class)
+                    .setParameter("trade_code", trade_code)
+                    .getSingleResult();
+            em.close();
+            if(inventories_count>0){
+                return "入力された商品情報はすでに存在しています。";
+            }
+        }
+        return "";
+    }
+
+    private static String validateTrade_name(String trade_name){
+        if(trade_name==null || trade_name.equals("")){
+            return "氏名を入力してください。";
+        }
+
+        return "";
+    }
+
+}
